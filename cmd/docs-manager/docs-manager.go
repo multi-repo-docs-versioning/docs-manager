7b52cf66d931de20ed671ec48425d52efcbdebb6
adibrastegarnia <adib.rastegarnia@gmail.com>
2019-10-16 22:39:21 -0400

    Add an example for test

--- cmd/docs-manager/docs-manager.go
+++ cmd/docs-manager/docs-manager.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	utils "github.com/multi-repo-docs-versioning/docs-manager/pkg/utils"
+	"gopkg.in/src-d/go-git.v4"
+)
 
 func main() {
-	fmt.Println("Test")
+	// Clone the given repository to the given directory
+	utils.Info("git clone https://github.com/src-d/go-git")
+
+	_, err := git.PlainClone("/tmp/foo", false, &git.CloneOptions{
+		URL:      "https://github.com/src-d/go-git",
+		Progress: os.Stdout,
+	})
+
+	utils.CheckIfError(err)
+
 }
